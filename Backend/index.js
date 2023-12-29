7fea09300f2c607b225410d7f3c08696012b9274
Anjali <2106290kiit.ac.in>
2023-12-29 13:39:55 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const mongodb=require('mongodb')
-
+const mongodb = require("mongodb");
+const path = require("path");
 const app = express();
 app.use(
   cors({
     origin: "*",
   })
 );
+app.use(express.static(path.join(__dirname, "../Frontend/build")));
 const PORT = process.env.PORT || 8000;
 
 const userRoutes = require("./Routes/Login");
 const blogRoutes = require("./Routes/Blog");
 
 const mongoose = require("mongoose");
-async function connecting(){
+async function connecting() {
   await mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Successfully Connected");
-  })
-  .catch((err) => {
-    console.log("Error while connecting" + err);
-  });
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Successfully Connected");
+    })
+    .catch((err) => {
+      console.log("Error while connecting" + err);
+    });
 }
-connecting()
+connecting();
 
 app.use(express.json());
 app.use("/", userRoutes);
 app.use("/blog", blogRoutes);
-app.get("*", (req, res) => {
-  res.status(404).send({ error: "Something went wrong" });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Frontend/build/index.html"));
 });
 app.listen(PORT, () => {
   console.log(`Server running on port: `, PORT);
